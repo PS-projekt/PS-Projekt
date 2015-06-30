d265d09d495d59e8cb7d08090335fe7cb87ca98a
Nemo94 <nero941208@gmail.com>
2015-06-30 11:36:00 +0200

    Poczatek programu

--- stroik.m
+++ stroik.m
@@ -1,2 +1,29 @@
 clear all;
 clc; 
+x=1;
+%czestotliwosci strun gitarowych
+E2f = 82.4069;
+A2f = 110;
+D3f = 146.832;
+G3f = 195.998;
+B3f = 246.942;
+E4f = 329.628;
+while x==1,
+	x=isempty (input ('Nacisnij enter, by rozpoczac dzialanie programu, badz zero, by wyjsc.'));
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+end
