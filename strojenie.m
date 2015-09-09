4e0cc06ab34dfd80b81906274d403db4037c63ac
EZKLU <EZKLU@users.noreply.github.com>
2015-09-09 15:36:33 +0200

    Update strojenie.m

--- strojenie.m
+++ strojenie.m
@@ -19,16 +19,18 @@ end
 
 max_f=i;
 
-%zakladamy, ze stroimy z dokladnoscia do 2Hz
-%ze prazek ma byc wiekszy niz 500 dla E2 i wiekszy niz 1000 to nie jestem
-%pewien, wyszlo mi z testow co sie dzieje empirycznych....
+fprintf('Znaleziona czestotliwosc wynosi:');
+            disp(max_f*fs/n);
+            fprintf('\n');
+%zakladamy, ze stroimy z dokladnoscia do 5Hz
+blad=5;
 if(wybor==1)
     fprintf('Stroimy strune E2.\n');
-    if(max_f*fs/n < E2-2) 
+    if(max_f*fs/n < E2-blad)
         fprintf('Nastroj do gory, roznica wynosi:');
         disp(E2-(max_f*fs/n));
         fprintf('\n');
-    elseif(E2+2 < max_f*fs/n)
+    elseif(E2+blad < max_f*fs/n)
             fprintf('Nastroj do dolu, roznica wynosi:');
             disp((max_f*fs/n)-E2);
             fprintf('\n');
@@ -38,11 +40,11 @@ if(wybor==1)
     
 elseif(wybor==2)
     fprintf('Stroimy strune A2.\n');
-    if(max_f*fs/n < A2-2) 
+    if(max_f*fs/n < A2-blad) 
         fprintf('Nastroj do gory, roznica wynosi:');
         disp(A2-(max_f*fs/n));
         fprintf('\n');
-    elseif(A2+2 < max_f*fs/n)
+    elseif(A2+blad < max_f*fs/n)
             fprintf('Nastroj do dolu, roznica wynosi:');
             disp((max_f*fs/n)-A2);
             fprintf('\n');
@@ -52,11 +54,11 @@ elseif(wybor==2)
     
    elseif(wybor==3)
     fprintf('Stroimy strune D3.\n');
-    if(max_f*fs/n < D3-2) 
+    if(max_f*fs/n < D3-blad) 
         fprintf('Nastroj do gory, roznica wynosi:');
         disp(D3-(max_f*fs/n));
         fprintf('\n');
-    elseif(D3+2 < max_f*fs/n)
+    elseif(D3+blad < max_f*fs/n)
             fprintf('Nastroj do dolu, roznica wynosi:');
             disp((max_f*fs/n)-D3);
             fprintf('\n');
@@ -66,11 +68,11 @@ elseif(wybor==2)
     
     elseif(wybor==4)
     fprintf('Stroimy strune G3.\n');
-    if(max_f*fs/n < G3-2) 
+    if(max_f*fs/n < G3-blad) 
         fprintf('Nastroj do gory, roznica wynosi:');
         disp(G3-(max_f*fs/n));
         fprintf('\n');
-    elseif(G3+2 < max_f*fs/n)
+    elseif(G3+blad < max_f*fs/n)
             fprintf('Nastroj do dolu, roznica wynosi:');
             disp((max_f*fs/n)-G3);
             fprintf('\n');
@@ -80,11 +82,11 @@ elseif(wybor==2)
     
    elseif(wybor==5)
     fprintf('Stroimy strune H3.\n');
-    if(max_f*fs/n < H3-2) 
+    if(max_f*fs/n < H3-blad) 
         fprintf('Nastroj do gory, roznica wynosi:');
         disp(H3-(max_f*fs/n));
         fprintf('\n');
-    elseif(H3+2 < max_f*fs/n)
+    elseif(H3+blad < max_f*fs/n)
             fprintf('Nastroj do dolu, roznica wynosi:');
             disp((max_f*fs/n)-H3);
             fprintf('\n');
@@ -94,11 +96,11 @@ elseif(wybor==2)
     
     elseif(wybor==6)
     fprintf('Stroimy strune E4.\n');
-    if(max_f*fs/n < E4-2) 
+    if(max_f*fs/n < E4-blad) 
         fprintf('Nastroj do gory, roznica wynosi:');
         disp(E4-(max_f*fs/n));
         fprintf('\n');
-    elseif(E4+2 < max_f*fs/n)
+    elseif(E4+blad < max_f*fs/n)
             fprintf('Nastroj do dolu, roznica wynosi:');
             disp((max_f*fs/n)-E4);
             fprintf('\n');
