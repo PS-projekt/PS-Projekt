f7bbdb1ec1fce311b63aa025388aa9e47a46b8cc
KenAdams465 <matiz191@gmail.com>
2015-09-10 12:16:59 +0200

    Update nagrywanie.m

--- nagrywanie.m
+++ nagrywanie.m
@@ -1,7 +1,7 @@
 function [y, fs]=nagrywanie(~)
 
 recObj = audiorecorder;
-fprintf('Zagraj na gitarze jak najblizej mikrofonu - czas na uderzenie struny to 5 sekund.\n')
+fprintf('Zagraj na gitarze jak najblizej mikrofonu - czas na uderzenie struny to 5 sekund.\n');
 recordblocking(recObj, 5);
 fprintf('Koniec nagrywania.\n');
 y = getaudiodata(recObj);
